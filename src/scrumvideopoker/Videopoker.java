27ebb18b888e62d7b9589422f37e1ebdee820b6b
Adam Hambe <adamhambe@gmail.com>
2018-12-05 11:47:15 +0100

    coded holdAndDeal

--- src/scrumvideopoker/Videopoker.java
+++ src/scrumvideopoker/Videopoker.java
@@ -1,28 +1,51 @@
 package scrumvideopoker;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Videopoker {
 	
-	ArrayList<Card> hand = new ArrayList<>();
+	ArrayList<Card> hand1 = new ArrayList<>();
+	ArrayList<Card> hand2 = new ArrayList<>();
+
 	Deck d = new Deck();
 
-	public void printHand() {
-		for(int i = 0; i < hand.size(); i++) {
-			System.out.println(hand.get(i).toString());
+	public void printHand1() {
+		for(int i = 0; i < hand1.size(); i++) {
+			System.out.println("Card " + (i + 1) + ": " + hand1.get(i).toString());
+		}
+	}
+	
+	public void printHand2() {
+		for(int i = 0; i < hand2.size(); i++) {
+			System.out.println("Card " + (i + 1) + ": " + hand2.get(i).toString());
 		}
 	}
 	
 //	Lägger till 5 kort till starthanden
-	public void starterHand() {
+	public void fillHand1() {
 		d.shuffle();
-		while (hand.size() < 5)  {
-			hand.add(d.draw());
+		while (hand1.size() < 5)  {
+			hand1.add(d.draw());
+		}
+	}
+	
+	public void fillHand2() {
+		d.shuffle();
+		while (hand2.size() < 5)  {
+			hand2.add(d.draw());
 		}
 	}
 	
 //	Holdar de kort som spelaren önskar. Dealar efter att önskade kort har holdats.
 	public void holdAndDeal() {
+		System.out.println("Which cards do you want to hold?");
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()) {
+			Card card = hand1.get(sc.nextInt() - 1);
+			hand2.add(card);
+		}
+		
 		
 	}
 
