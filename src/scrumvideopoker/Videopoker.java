b12d46dc6c7038951a98aec468759a51af506c8b
Adam Hambe <adamhambe@gmail.com>
2018-12-05 12:03:51 +0100

    Latest update #1

--- src/scrumvideopoker/Videopoker.java
+++ src/scrumvideopoker/Videopoker.java
@@ -60,15 +60,5 @@ public class Videopoker {
 		
 		
 	}
-
 	
-//	Lägger till 5 kort till starthanden
-	public void starterHand() {
-		
-	}
-
-//	Holdar de kort som spelaren önskar. Dealar efter att önskade kort har holdats.
-	public void holdAndDeal() {
-		
-	}
 }
